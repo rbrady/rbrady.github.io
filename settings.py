21a0951e11829177e62b60f27668484d79e79fd4
Ryan Brady <ryan@ryanbrady.org>
2013-04-17 18:20:09 -0400

    added settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+AUTHOR = 'Ryan Brady'
+GITHUB_URL = 'http://github.com/rbrady'
+SITEURL = 'http://rbrady.github.io'
+SITENAME = 'Ryan Brady'
+SOCIAL = (('twitter', 'http://twitter.com/rjbrady'),
+	  ('github', 'http://github.com/rbrady'),)
+TAG_FEED = 'feeds/%s.atom.xml'
+THEME = 'notmyidea'
+TWITTER_USERNAME = 'rjbrady'
